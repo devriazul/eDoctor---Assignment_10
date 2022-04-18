c024338285837396a7ad4a1b3fe98794c4251d27
devriazul <engr.riazul@gmail.com>
2022-04-18 23:08:35 +0600

    about page added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './About.css'
 const About = () => {
     return (
-        <div>
+        <div className="container row">
             <h2>About</h2>
+            <div className="col-md-6">
+                <h4>Md Riazul Islam</h4>
+                <p>I am a Full Stack Web Developer. I am working with React, PHP, Laravel and MySQL. I also have experience in developing static/dynamic websites using HTML5, CSS3, Bootstrap, Tailwind, JavaScript (ES6) and Wordpress. I am a quick learner. I believe in hard work and efficiency. That's why I am always ready to accomplish any task by working hard. I always focus on learning new technology. My goal is to become A World-Class Professional Web Developer.</p>
+            </div>
+            <div className="col-md-6">
+                <img src="https://avatars.githubusercontent.com/u/24354468?v=4" alt="" />
+            </div>
         </div>
     );
 };
